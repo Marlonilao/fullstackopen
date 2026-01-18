088928974f1f3671b7cf918c2aa6ed6f3ff36a7a
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-01-18 20:25:02 +0800

    Add part2 exercise 2.6: The Phonebook step 1

---
+++ part2/thePhonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function App() {
+  const [persons, setPersons] = useState([{ name: "Marlon Ilao" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name:{" "}
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.length > 0
+          ? persons.map((person) => <p key={person.name}>{person.name}</p>)
+          : null}
+      </div>
+    </div>
+  );
+}
+
+export default App;
