c5f3d96eec298cfd6ccde40718bd095aa755db88
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-01-19 08:37:13 +0800

    Add part2 exercise 2.8: The phonebook step 3

--- part2/thePhonebook/src/App.jsx
+++ part2/thePhonebook/src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Marlon Ilao" }]);
+  const [persons, setPersons] = useState([
+    { name: "Marlon Ilao", number: "12345" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       setNewName("");
+      setNewNumber("");
       return;
     }
-    setPersons([...persons, { name: newName }]);
+    setPersons([...persons, { name: newName, number: newNumber }]);
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -28,13 +33,25 @@ function App() {
           />
         </div>
         <div>
+          number:{" "}
+          <input
+            type="text"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.length > 0
-          ? persons.map((person) => <p key={person.name}>{person.name}</p>)
+          ? persons.map((person) => (
+              <p key={person.name}>
+                {person.name} {person.number}
+              </p>
+            ))
           : null}
       </div>
     </div>
