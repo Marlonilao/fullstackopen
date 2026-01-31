adbe1e5d2dafbfaee886f66fd8a27579029e0249
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-01-31 19:48:28 +0800

    Add part3 exercise 3.8*: Phonebook backend step 8

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -3,7 +3,25 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", function (req, res) {
+  return req.body;
+});
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(tokens.body(req, res)),
+    ].join(" ");
+  }),
+);
 
 let persons = [
   {
