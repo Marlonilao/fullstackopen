266097f051e3b38630e253a48e085b43dfa3dac2
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-02-07 20:50:40 +0800

    Add part3 exercise 3.9: Phonebook backend step 9

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 morgan.token("body", function (req, res) {
   return req.body;
 });
@@ -99,6 +104,6 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 app.listen(PORT);
 console.log(`server running on port ${PORT}`);
