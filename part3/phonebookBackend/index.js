d57e251faacf2c4288490e8fba641808b4008877
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-02-17 20:56:55 +0800

    Add part3 exercise 3.15: Phonebook database, step 3

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -58,11 +58,14 @@ app.get("/api/persons/:id", (request, response) => {
   //   }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons.filter((person) => person.id !== id);
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error));
+  // const id = request.params.id;
+  // persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
+  // response.status(204).end();
 });
 
 app.post("/api/persons", (request, response) => {
