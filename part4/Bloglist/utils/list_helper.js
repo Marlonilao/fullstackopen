fa21d618f398c47ef0fdca0912095af823fa7f4f
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-02-26 18:55:06 +0800

    Add part4 exercise 4.7*

--- part4/Bloglist/utils/list_helper.js
+++ part4/Bloglist/utils/list_helper.js
@@ -39,4 +39,27 @@ const mostBlogs = (blogs) => {
   return blogs.length === 0 ? null : result
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
+const mostLikes = (blogs) => {
+  const likeCounts = {}
+
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].author in likeCounts) {
+      likeCounts[blogs[i].author] = likeCounts[blogs[i].author] + blogs[i].likes
+    } else {
+      likeCounts[blogs[i].author] = blogs[i].likes
+    }
+  }
+
+  const result = { author: '', likes: 0 }
+
+  for (const author in likeCounts) {
+    if (likeCounts[author] > result.likes) {
+      result.author = `${author}`
+      result.likes = likeCounts[author]
+    }
+  }
+
+  return blogs.length === 0 ? null : result
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
