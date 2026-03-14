bf8f60a8268274e181a28d32c4d3c3c05ce02f16
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-03-14 17:11:36 +0800

    add part4 exercise 4.19

--- part4/Bloglist/controller/blogs.js
+++ part4/Bloglist/controller/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,20 +18,24 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
   } else if (!body.title || !body.url) {
-    response.status(400).json({ error: 'title or url missing' })
+    return response.status(400).json({ error: 'title or url missing' })
   } else {
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes,
-      user: body.userId,
+      user: decodedToken.id,
     })
 
     const savedBlog = await blog.save()
