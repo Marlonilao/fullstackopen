89455ec432e5055b58cc070f8897a97ba14e4ec8
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-03-14 09:42:05 +0800

    add part4 exercise 4.17

--- part4/Bloglist/controller/blogs.js
+++ part4/Bloglist/controller/blogs.js
@@ -1,18 +1,35 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  if (!body.title || !body.url) {
+
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  } else if (!body.title || !body.url) {
     response.status(400).json({ error: 'title or url missing' })
   } else {
-    const blog = new Blog(body)
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: body.userId,
+    })
+
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id.toString())
+    await user.save()
+
     response.status(201).json(savedBlog)
   }
 })
