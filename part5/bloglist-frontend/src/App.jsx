5a4a58419a9b63913bb87432b2fb661937c413cd
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-03-19 08:27:26 +0800

    add part5 exercise 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -14,13 +14,27 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (e) => {
     e.preventDefault()
 
     const user = await loginService.login({ username, password })
+    window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
     setUser(user)
     setUsername('')
     setPassword('')
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   return (
@@ -43,9 +57,12 @@ const App = () => {
       {user && (
         <div>
           <h2>blogs</h2>
-
-          <p>{user.name} logged in</p>
-
+          <p>
+            {user.name} logged in{' '}
+            <button type='button' onClick={handleLogout}>
+              logout
+            </button>
+          </p>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
