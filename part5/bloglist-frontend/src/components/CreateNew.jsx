93e0ebcf016a3c882687b27c177451414de636fd
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-03-21 09:17:42 +0800

    add part5 exercise 5.5

--- part5/bloglist-frontend/src/components/CreateNew.jsx
+++ part5/bloglist-frontend/src/components/CreateNew.jsx
@@ -1,39 +1,43 @@
+import Togglable from './Togglable'
+
 const CreateNew = (props) => {
   return (
-    <form onSubmit={props.handleCreateNew}>
-      <h2>Create New</h2>
-      <div>
-        <label>
-          title:{' '}
-          <input
-            type='text'
-            value={props.title}
-            onChange={props.handleTitleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          author:{' '}
-          <input
-            type='text'
-            value={props.author}
-            onChange={props.handleAuthorChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          url:{' '}
-          <input
-            type='text'
-            value={props.url}
-            onChange={props.handleUrlChange}
-          />
-        </label>
-      </div>
-      <button type='submit'>Create</button>
-    </form>
+    <Togglable buttonLabel='create new blog'>
+      <form onSubmit={props.handleCreateNew}>
+        <h2>Create New</h2>
+        <div>
+          <label>
+            title:{' '}
+            <input
+              type='text'
+              value={props.title}
+              onChange={props.handleTitleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            author:{' '}
+            <input
+              type='text'
+              value={props.author}
+              onChange={props.handleAuthorChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            url:{' '}
+            <input
+              type='text'
+              value={props.url}
+              onChange={props.handleUrlChange}
+            />
+          </label>
+        </div>
+        <button type='submit'>Create</button>
+      </form>
+    </Togglable>
   )
 }
 
