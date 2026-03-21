9878fb896ca3afbf7c37eccb6978acbc87be3098
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-03-21 16:12:03 +0800

    add part5 exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <li>
-    Title: {blog.title} / Author: {blog.author}
-  </li>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      {isExpanded ? (
+        <>
+          <p>
+            Title: {blog.title}{' '}
+            <button onClick={() => setIsExpanded(!isExpanded)}>hide</button>
+          </p>
+          <p>Url: {blog.url}</p>
+          <p>
+            Likes: {blog.likes} <button>like</button>
+          </p>
+          <p>Author: {blog.author}</p>
+        </>
+      ) : (
+        <>
+          Title: {blog.title} / Author: {blog.author}{' '}
+          <button onClick={() => setIsExpanded(!isExpanded)}>View</button>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
