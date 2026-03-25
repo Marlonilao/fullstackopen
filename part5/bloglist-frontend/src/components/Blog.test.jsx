f024f696be5603d9340e56c8254104be2fbedbb6
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-03-25 09:33:03 +0800

    add part5 exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('renders title and author but not url or likes by default', () => {
   const blog = {
@@ -21,4 +22,27 @@ test('renders title and author but not url or likes by default', () => {
   expect(titleAndAuthor).toBeInTheDocument()
   expect(url).not.toBeInTheDocument()
   expect(likes).not.toBeInTheDocument()
+})
+
+test("blog's URL and number of likes are shown when the button controlling the shown details has been clicked", async () => {
+  const blog = {
+    title: 'blog title',
+    author: 'blog author',
+    url: 'blog url',
+    likes: 1,
+    user: { username: 'asd', name: 'asd' },
+  }
+
+  const userObject = { username: 'asd', name: 'asd' }
+
+  const { container } = render(<Blog blog={blog} user={userObject} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  const url = container.querySelector('.url')
+  const likes = container.querySelector('.likes')
+  expect(url).toBeInTheDocument()
+  expect(likes).toBeInTheDocument()
 })
